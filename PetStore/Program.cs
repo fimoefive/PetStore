ddfda59020f03eecfbe6227b1ebdb5d072f50a0f
Martin Sisk <Brothermj@hotmail.com>
2021-08-03 15:02:16 -0500

    Added Bark and Eat Method in Dod.cs file.

--- PetStore/Program.cs
+++ PetStore/Program.cs
@@ -1,13 +1,38 @@
 ﻿using PetStore.Pets;
 using System;
 
+
 namespace PetStore
 {
     class Program
     {
+        // static means shared across all instances
         static void Main(string[] args)
         {
+            var melba = new Dog();
+            melba.Name = "Melba";
+            melba.Size = "Medium";
+            melba.Weight = 60;
+            // this won't work, can't dynamically add properties to a class in C#
+            // melba.CoatType = "Short";
+
+            var melba = new Dog("Melba", 60, "Medium");
+            melba.Bark();
+
+            // property is privately settable only
+            // melba.Weight -= 15;
+
+            var barley = new Dog();
+            barley.Name = "Barley";
+            barley.Size = "Medium";
+            barley.Weight = 50;
            
+            var barley = new Dog("Barley", 50, "Medium");
+            barley.Bark();
+            barley.Eat("table scraps");
+
+
+
         }
     }
 }
